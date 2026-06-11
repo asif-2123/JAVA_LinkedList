9c3fa115178f3591280d833f848cc5348b991788
Md Asif <asif212327@gmail.com>
2026-06-12 00:51:31 +0530

    Add files via upload

---
+++ LinkedList/Add_Last.java
@@ -0,0 +1,45 @@
+import java.util.*;
+public class Add_Last {
+    public static class Node{
+        int data;
+        Node next;
+        public Node(int data){
+            this.data=data;
+            this.next=null;
+        }
+    }
+    public static Node head;
+    public static Node tail;
+    public void addLast(int data){
+        Node newNode=new Node(data);
+        if(head==null){
+            head=tail=newNode;
+            return;
+        }
+        tail.next=newNode;
+        tail=newNode;
+    }
+    public void print(){
+        if(head==null){
+            System.out.println("LL is empty");
+            return;
+        }
+        Node temp=head;
+        while(temp!=null){
+            System.out.print(temp.data+"->");
+            temp=temp.next;
+        }
+        System.out.println("null");
+    }
+
+    public static void main(String args[]){
+        Add_Last ll=new Add_Last();
+        ll.print();
+        ll.addLast(2);
+        ll.print();
+        ll.addLast(1);
+        ll.print();
+
+    }
+} 
+
