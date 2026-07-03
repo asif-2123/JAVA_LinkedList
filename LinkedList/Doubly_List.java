0922097d023b5b54010e95770fff009a2b061f52
Md Asif <asif212327@gmail.com>
2026-07-03 22:41:47 +0530

    Add files via upload

---
+++ LinkedList/Doubly_List.java
@@ -0,0 +1,69 @@
+import java.util.*;
+public class Doubly_List {
+    public static class Node{
+        int data;
+        Node next;
+        Node prev;
+        public Node(int data){
+            this.data=data;
+            this.next=null;
+            this.prev=null;
+        }
+    }
+    public static Node head;
+    public static Node tail;
+    public static int size;
+    public static void addFirst(int data){
+        Node newNode=new Node(data);
+        size++;
+        if(head==null){
+            head=tail=newNode;
+            return;
+        }
+        newNode.next=head;
+        newNode.prev=null;
+        head.prev=newNode;
+        head=newNode;
+    }
+    public static void addLast(int data){
+        Node newNode=new Node(data);
+        size++;
+        if(head==null){
+            head=tail=newNode;
+            return;
+        }
+        tail.next=newNode;
+        newNode.next=null;
+        newNode.prev=tail;
+        tail=newNode;
+    }
+    public static void print(){
+        if(head==null){
+            System.out.println("LL is empty");
+            return;
+        }
+        Node temp=head;
+        while(temp!=null){
+            System.out.print(temp.data+"<->");
+            temp=temp.next;
+        }
+        System.out.println("null");
+    }
+    public static void main(String args[]){
+        Doubly_List ll=new Doubly_List();
+        ll.addFirst(1);
+        ll.addFirst(2);
+        ll.addFirst(3);
+        ll.addFirst(4);
+        ll.addFirst(5);
+        ll.print();
+        System.out.println(size);
+        ll.addLast(1);
+        ll.addLast(2);
+        ll.addLast(3);
+        ll.addLast(4);
+        ll.addLast(5);
+        ll.print();
+        System.out.println(size);
+    }
+}
