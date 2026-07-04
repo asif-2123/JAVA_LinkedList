1ae1427c8fb802995d1986cac66a343a8ed73fb6
Md Asif <asif212327@gmail.com>
2026-07-04 23:35:12 +0530

    Add files via upload

---
+++ LinkedList/Doubly_Reverse.java
@@ -0,0 +1,62 @@
+import java.util.*;
+public class Doubly_Reverse {
+    public static class Node{
+        int data;
+        Node next;
+        Node prev;
+        public Node(int data){
+            this.data=data;
+            this.next=null;
+            this.prev=null;
+        }
+    }
+    public static Node head;
+    public static Node tail;
+    public static void addFirst(int data){
+        Node newNode=new Node(data);
+        if(head==null){
+            head=tail=newNode;
+            return;
+        }
+        newNode.next=head;
+        head.prev=newNode;
+        newNode.prev=null;
+        head=newNode;
+    }
+    public static void reverse(){
+        Node curr=head;
+        Node prev=null;
+        Node next;
+        while(curr!=null){
+            next=curr.next;
+            curr.next=prev;
+            curr.prev=next;
+            prev=curr;
+            curr=next;
+        }
+        head=prev;
+    }
+    public static void print(){
+        if(head==null){
+            System.out.println("LL is empty");
+            return;
+        }
+        Node temp=head;
+        while(temp!=null){
+            System.out.print(temp.data+"<->");
+            temp=temp.next;
+        }
+        System.out.println("null");
+    }
+    public static void main(String args[]){
+        Doubly_Reverse ll=new Doubly_Reverse();
+        ll.addFirst(1);
+        ll.addFirst(2);
+        ll.addFirst(3);
+        ll.addFirst(4);
+        ll.addFirst(5);
+        ll.print();
+        ll.reverse();
+        ll.print();
+    }
+}
