da79d173f9da712f839eab81ef9f3b762059ee93
Md Asif <asif212327@gmail.com>
2026-07-12 00:04:16 +0530

    Add files via upload

---
+++ LinkedList/Reverse.java
@@ -0,0 +1,56 @@
+import java.util.*;
+public class Reverse {
+    public static class Node{
+        int data;
+        Node next;
+        public Node(int data){
+            this.data=data;
+            this.next=null;
+        }
+    }
+    public static Node head;
+    public static Node tail;
+    public static void addFirst(int data){
+        Node newNode=new Node(data);
+        if(head==null){
+            head=tail=newNode;
+            return;
+        }
+        newNode.next=head;
+        head=newNode;
+    }
+    public static void reverse(){
+        Node prev=null;
+        Node curr=tail=head;
+        Node next;
+        while(curr!=null){
+            next=curr.next;
+            curr.next=prev;
+            prev=curr;
+            curr=next;
+        }head=prev;
+    }
+    public static void print(){
+        if(head==null){
+            System.out.println("LL is empty");
+            return;
+        }
+        Node temp=head;
+        while(temp!=null){
+            System.out.print(temp.data+"->");
+            temp=temp.next;
+        }
+        System.out.println("null");
+    }
+    public static void main(String args[]){
+        Reverse ll=new Reverse();
+        ll.addFirst(1);
+        ll.addFirst(2);
+        ll.addFirst(3);
+        ll.addFirst(4);
+        ll.addFirst(5);
+        ll.print();
+        ll.reverse();
+        ll.print();
+    }
+}
