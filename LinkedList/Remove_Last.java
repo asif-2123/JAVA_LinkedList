ccc65659837702dd13e8b1273ad6a0df55e5d256
Md Asif <asif212327@gmail.com>
2026-07-11 11:33:33 +0530

    Add files via upload

---
+++ LinkedList/Remove_Last.java
@@ -0,0 +1,71 @@
+import java.util.*;
+public class Remove_Last {
+    public static class Node{
+        int data;
+        Node next;
+        public Node(int data){
+            this.data=data;
+            this.next=null;
+        }
+    }
+    public static Node head;
+    public static Node tail;
+    public static int size;
+
+    public static void addFirst(int data){
+        Node newNode=new Node(data);
+        size++;
+        if(head==null){
+            head=tail=newNode;
+            return;
+        }
+        newNode.next=head;
+        head=newNode;
+    }
+    public static int removeLast(){
+        if(size==0){
+            System.out.println("LL is empty");
+            size=0;
+            return Integer.MIN_VALUE;
+        }else if(size==1){
+            int val=head.data;
+            head=tail=null;
+            size--;
+            return val;
+        }
+        Node prev=head;
+        for(int i=0;i<size-2;i++){
+            prev=prev.next;
+        }
+        int val=prev.next.data;
+        prev.next=null;
+        tail=prev;
+        size--;
+        return val;
+    }    
+    public static void print(){
+        if(head==null){
+            System.out.println("LL is empty");
+            return;
+        }
+        Node temp=head;
+        while(temp!=null){
+            System.out.print(temp.data+"->");
+            temp=temp.next;
+        }
+        System.out.println("null");
+    }
+    public static void main(String args[]){
+        Remove_Last ll=new Remove_Last();
+        ll.addFirst(1);
+        ll.addFirst(2);
+        ll.addFirst(3);
+        ll.addFirst(4);
+        ll.addFirst(5);
+        ll.print();
+        System.out.println("Size before removing: "+size);
+        ll.removeLast();
+        ll.print();
+        System.out.println("Size after removing: "+size);
+    }
+}
