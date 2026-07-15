e72a847b170939d6dcd8fcfe3a7398eeee5f3e0d
Md Asif <asif212327@gmail.com>
2026-07-15 10:55:23 +0530

    Add files via upload

---
+++ LinkedList/Search_rec.java
@@ -0,0 +1,60 @@
+import java.util.*;
+public class Search_rec {
+    public static class Node{
+        int data;
+        Node next;
+        public Node(int data){
+            this.data=data;
+            this.next=null;
+        }
+    }
+    public static Node head;
+    public static Node tail;
+    public static int size;
+    public static void addFirst(int data){
+        Node newNode=new Node(data);
+        size++;
+        if(head==null){
+            head=tail=newNode;
+            return;
+        }
+        newNode.next=head;
+        head=newNode;
+    }
+    public static int helper(Node head,int key){
+        if(head==null){
+            return -1;
+        }
+        if(head.data==key){
+            return 0;
+        }
+        int idx=helper(head.next,key);
+        if(idx==-1){
+            return -1;
+        }
+        return idx+1;
+    }
+    public static int search(int key){
+        return helper(head,key);
+    }
+    public static void print(){
+        if(head==null){
+            System.out.println("LL is empty");
+        }
+        Node temp=head;
+        while(temp!=null){
+            System.out.print(temp.data+"->");
+            temp=temp.next;
+        }System.out.println("null");
+    }
+    public static void main(String[] args) {
+        Search_rec ll = new Search_rec();
+        ll.addFirst(1);
+        ll.addFirst(2);
+        ll.addFirst(3);
+        ll.addFirst(4);
+        ll.addFirst(5);
+        ll.print();
+        System.out.println(search(10));
+    }
+}
