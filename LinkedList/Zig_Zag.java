45ccd99ba35704f3e220d59909c2538a70249d3b
Md Asif <asif212327@gmail.com>
2026-07-17 00:25:54 +0530

    Add files via upload

---
+++ LinkedList/Zig_Zag.java
@@ -0,0 +1,80 @@
+import java.util.*;
+public class Zig_Zag {
+    public static class Node{
+        int data;
+        Node next;
+        public Node(int data){
+            this.data=data;
+            this.next=null;
+        }
+    }
+    public static Node head;
+    public static Node tail;
+    public static void addFirst(int data){
+        Node newNode=new Node(data);
+        if(head==null){
+            head=tail=newNode;
+            return;
+        }
+        newNode.next=head;
+        head=newNode;
+    }
+    public static void zigZag(){
+        //get mid
+        Node slow=head;
+        Node fast=head.next;
+        while(fast!=null && fast.next!=null){
+            slow=slow.next;
+            fast=fast.next.next;
+        }
+        Node mid=slow;
+        // reverse
+        Node curr=mid.next;
+        mid.next=null;
+        Node prev=null;
+        Node next;
+        while(curr!=null){
+            next=curr.next;
+            curr.next=prev;
+            prev=curr;
+            curr=next;
+        }
+
+        Node left=head;
+        Node right=prev;
+        Node nextL;
+        Node nextR;
+        while(left!=null && right!=null){
+            nextL=left.next;
+            left.next=right;
+            nextR=right.next;
+            right.next=nextL;
+
+            left=nextL;
+            right=nextR;
+        }
+    
+    }
+    public static void print(){
+        if(head==null){
+            System.out.println("LL is empty");
+        }
+        Node temp=head;
+        while(temp!=null){
+            System.out.print(temp.data+"->");
+            temp=temp.next;
+        }
+        System.out.println("null");
+    }
+    public static void main(String args[]){
+        Zig_Zag ll=new Zig_Zag();
+        ll.addFirst(5);
+        ll.addFirst(4);
+        ll.addFirst(3);
+        ll.addFirst(2);
+        ll.addFirst(1);
+        ll.print();
+        ll.zigZag();
+        ll.print();
+    }
+}
